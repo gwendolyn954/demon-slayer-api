fa9f9873faf67ac78ebfeb20db81c8d749f038d8
Gwendolyn Asani <107776357+gwendolyn954@users.noreply.github.com>
2023-06-16 22:07:38 -0400

    added new public and character js folders

---
+++ characters.js
@@ -0,0 +1,47 @@
+const characters = [
+    {
+        'tanjiro': {
+            'fullName': 'Tanjiro Kamado',
+            'race': 'human',
+            'affiliation': 'Demon Slayer Corps',
+            'combatStyle': 'Water Breathing and Sun Breathing'
+        },
+    
+        'nezuko': {
+            'fullName': 'Nezuko Kamado',
+            'race': 'demon',
+            'affiliation': 'Demon Slayer Corps (formerly)',
+            'combatStyle': 'Blood Demon Art'
+        },
+    
+        'zenitsu': {
+            'fullName': 'Zenitsu Agatsuma',
+            'race': 'human',
+            'affiliation': 'Demon Slayer Corps',
+            'combatStyle': 'Thunder Breathing'
+        },
+    
+        'inosuke': {
+            'fullName': 'Inosuke Hashibira',
+            'race': 'human',
+            'affiliation': 'Demon Slayer Corps',
+            'combatStyle': 'Beast Breathing'
+        },
+    
+        'muzan': {
+            'fullName': 'Muzan Kibutsuji',
+            'race': 'demon',
+            'affiliation': 'Twelve Kizuki',
+            'combatStyle': 'none'
+        },
+    
+        'unknown': {
+            'fullName': 'unknown',
+            'race': 'unknown',
+            'affiliation': 'unknown',
+            'combatStyle': 'unknown'
+        }
+    }
+
+]
+
