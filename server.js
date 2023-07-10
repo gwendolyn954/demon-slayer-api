d9a41c971fddc5fd5b96862900771b9d032e4de8
Gwendolyn Asani <107776357+gwendolyn954@users.noreply.github.com>
2023-07-10 13:18:39 -0400

    minor styling fix and refactor to server

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ app.set('view engine', 'ejs')
 app.use(cors())
 app.use(express.static('public'))
 
-
-
 app.get('/', (req, res) => {
     res.render('index', { characters: [] }); // Render empty character list initially
   });
